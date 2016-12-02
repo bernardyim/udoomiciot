6522195fdfe2922d3e43dcbe31c74910111378ca
Bernard Yim <bernard_yim@mymail.sutd.edu.sg>
2016-12-02 17:04:34 +0800

    finalise template split according to individual roles

--- UdooMicIot/app/src/main/java/com/example/bernard/udoomiciot/MainActivity.java
+++ UdooMicIot/app/src/main/java/com/example/bernard/udoomiciot/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.bernard.udoomiciot;
 
+import android.os.AsyncTask;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,16 +14,60 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    @Override
+    public void onResume() { // activity is in foreground (running state)
+        super.onResume();
+        new startRecordingAsync().execute();
+    }
+    @Override
+    public void onPause() {  // activity is not in the foreground but still alive
+        super.onPause();
+    }
+    @Override
+    public void onDestroy() { // activity is about to be destroyed
+        super.onDestroy();  // call super class implementation of onDestroy
+    }
 
-    //TODO:
-    //getAveAmp
-    //getTime
-    //send(String identifier) -> Send to url specified with identifier, and the parameters obtained in getTime and getAveAmp
 
-    //returns the average amplitude of sound received in the past 1 second. Implements the getAmp() method
-    public double getAveAmp(){return 0.0;}
+    public void startRecording(View v) {
+        new startRecordingAsync().execute();
+    }
 
-    public void startRecording(View v){
+
+    private class startRecordingAsync extends AsyncTask<Void, String, Void> {
         //start recording data and sending data here
+        //TODO:
+        //getAveAmp <- EIROS HERE
+        //getTime
+        //send
+
+        private boolean running = true;
+
+        public void pause(){
+            running = false;
+        }
+
+        protected Void doInBackground(Void... params) {
+            while (running){
+                //getTime()
+                //getAveAmp()
+                //send(String identifier) -> Send to url specified with identifier, and the parameters obtained in getTime and getAveAmp
+                publishProgress("some message");
+
+            }
+            return null;
+        }
+
+        //EIROS THIS IS YOUR SPACE DO YOUR JOB LOLOLOL
+        //returns the average amplitude of sound received in the past 1 second. Implements the getAmp() method
+        public Double getAveAmp() {
+            return 0.0;
+        }
+
+
+        protected void onProgressUpdate(String... progress) {
+            //TODO:
+            //update the textview with the latest amplitude
+        }
     }
 }
