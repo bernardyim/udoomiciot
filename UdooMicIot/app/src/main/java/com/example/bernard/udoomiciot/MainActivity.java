68f9b11252e519571afe78753462ea5103b5bf7e
Bernard Yim <bernard_yim@mymail.sutd.edu.sg>
2016-12-06 13:01:56 +0800

    finish first attempt at sockets, awaiting test server

--- UdooMicIot/app/src/main/java/com/example/bernard/udoomiciot/MainActivity.java
+++ UdooMicIot/app/src/main/java/com/example/bernard/udoomiciot/MainActivity.java
@@ -3,31 +3,32 @@ package com.example.bernard.udoomiciot;
 import android.os.AsyncTask;
 import android.os.Handler;
 import android.os.Message;
+import android.support.v4.app.ShareCompat;
+import android.support.v4.widget.TextViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.net.Socket;
+
+import static android.os.SystemClock.uptimeMillis;
 
 public class MainActivity extends AppCompatActivity {
+    final static int udooID = 2;
+    final static String herokuUrl = "http://www.dummyurl12341515.com";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView deviceData = (TextView)findViewById(R.id.deviceData);
+        deviceData.setText(Integer.toString(udooID));
     }
-    @Override
-    public void onResume() { // activity is in foreground (running state)
-        super.onResume();
-        new startRecordingAsync().execute();
-    }
-    @Override
-    public void onPause() {  // activity is not in the foreground but still alive
-        super.onPause();
-    }
-    @Override
-    public void onDestroy() { // activity is about to be destroyed
-        super.onDestroy();  // call super class implementation of onDestroy
-    }
-
 
     public void startRecording(View v) {
         new startRecordingAsync().execute();
@@ -38,23 +39,34 @@ public class MainActivity extends AppCompatActivity {
         //start recording data and sending data here
         //TODO:
         //getAveAmp <- EIROS HERE
-        //getTime
-        //send
 
         private boolean running = true;
 
-        public void pause(){
-            running = false;
-        }
-
         protected Void doInBackground(Void... params) {
-            while (running){
-                //getTime()
-                //getAveAmp()
-                //send(String identifier) -> Send to url specified with identifier, and the parameters obtained in getTime and getAveAmp
-                publishProgress("some message");
+            Socket socket = null;
+            OutputStream outputStream;
+            try {
+                socket = new Socket(herokuUrl,80);
+                while (running){
+                //getTime(), getAveAmp(), and parse message all in one! :)
+                String udoomsg = "time="+uptimeMillis()+";aveAmp="+getAveAmp();
+                    delay(1000);
 
-            }
+                //send(String identifier) -> Open socket connection to url specified with identifier, and the parameters obtained in getTime and getAveAmp
+
+                    /*
+                    outputStream = socket.getOutputStream();
+                    PrintStream printStream = new PrintStream(outputStream);
+                    printStream.print(udoomsg);
+                    printStream.close();
+                    */
+
+                    //update to textview that which we published
+                    publishProgress(udoomsg);
+
+                }
+
+            } catch (IOException e) {e.printStackTrace(); System.out.print("Prolly cannot socket");}
             return null;
         }
 
@@ -64,10 +76,10 @@ public class MainActivity extends AppCompatActivity {
             return 0.0;
         }
 
-
         protected void onProgressUpdate(String... progress) {
-            //TODO:
             //update the textview with the latest amplitude
+            TextView logData = (TextView)findViewById(R.id.logData);
+            logData.setText(progress[0]);
         }
     }
 }
